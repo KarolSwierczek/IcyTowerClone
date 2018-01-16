c7f322afb7a7827a97cc2be97d24d664df47a660
Karol Świerczek <karolswierczek5@gmail.com>
2018-01-16 13:20:43 +0100

    Added super jumps back, resized the stage.

--- Icy Tower/Assets/Scripts/Controller.cs
+++ Icy Tower/Assets/Scripts/Controller.cs
@@ -10,12 +10,15 @@ public class Controller : MonoBehaviour {
     public float ReverseSpeed = 0.6f;
     public float JumpTime = 3.0f;
     public float JumpHeight = 5.0f;
-    public float SuperJumpHeight = 15.0f;
+    //Flow is how much horizontal velocity affects jumping
+    public float Flow = 3.0f;
+    public float FlowThreshold = 0.7f;
     public float GroundDistance = 0.2f;
     public float Gravity = 75.0f;
     public LayerMask Ground;
 
     private Rigidbody _body;
+    private Collider _collider;
     private float _input = 0.0f;
     private bool _isGrounded = true;
     private Transform _groundChecker;
@@ -23,11 +26,13 @@ public class Controller : MonoBehaviour {
     private float _verticalVelocity = 0.0f;
     private bool _isJumping = false;
     private float _timer = 0.0f;
+    private float _jumpModifier = 0.0f;
 
 
     void Start()
     {
         _body = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
         _groundChecker = transform.GetChild(0);
         _timer = 0;
     }
@@ -41,16 +46,8 @@ public class Controller : MonoBehaviour {
         {
             //Jumping with constant velocity. 
             //Can't use AddForce, bc sometimes the vertical velocity doesn't reset fast enaugh when Player is grounded and it affects jumping velocity.
-            if (_verticalVelocity >= 25)
-            {
-                _verticalVelocity = SuperJumpHeight / JumpTime;
-            }
-            else
-            {
-                _verticalVelocity = JumpHeight / JumpTime;
-            }
-            _body.velocity = new Vector3(_body.velocity.x, _verticalVelocity, 0.0f);
-            
+            _verticalVelocity = JumpHeight / JumpTime * (1 + _jumpModifier); 
+            _body.velocity = new Vector3(_body.velocity.x, _verticalVelocity, 0.0f);  
             _isJumping = true;
         }
 
@@ -58,7 +55,7 @@ public class Controller : MonoBehaviour {
         {
             //When the timer runs out, "gravity" is applied.
             _timer += Time.deltaTime;
-            if (_timer >= JumpTime)
+            if (_timer >= JumpTime * (1 + 0.5f * _jumpModifier))
             {
                 _isJumping = false;
                 _timer = 0;
@@ -69,10 +66,14 @@ public class Controller : MonoBehaviour {
 
     private void FixedUpdate()
     {
+        //jump modifier update.
+        _jumpModifier = UpdateJumpModifier(_horizontalVelocity);
+        _collider.material.bounciness = 0.6f + 0.4f*_jumpModifier / Flow;
+
         if (_input * _horizontalVelocity < 0)
         {
-            //when trying to change direction, increase acceleration proportionally to current horizontal velocity value.
-            _input += Mathf.Abs(_body.velocity.x) * ReverseSpeed * _input;;
+            //When trying to change direction, increase acceleration proportionally to current horizontal velocity value.
+            _input += Mathf.Abs(_body.velocity.x) * ReverseSpeed * _input;    
         }
         
         if (_input == 0) {
@@ -111,6 +112,16 @@ public class Controller : MonoBehaviour {
         {
             return limit*Mathf.Sign(speed);
         }
+    }
+
+    //How much current velocity goes over a threshold.
+    private float UpdateJumpModifier(float velocity)
+    {
+        if (Mathf.Abs(velocity) > FlowThreshold * MaxSpeed)
+        {
+            return Flow*(Mathf.Abs(velocity) - MaxSpeed * FlowThreshold) / (MaxSpeed * FlowThreshold);
+        }
+        return 0;
     }
 
 }
