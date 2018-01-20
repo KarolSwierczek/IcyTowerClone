15dc51d8c2a99047204004a850495195d8d33a06
Karol Świerczek <karolswierczek5@gmail.com>
2018-01-20 16:56:02 +0100

    unversioned fles.

---
+++ Icy Tower/Assets/Scripts/Player Scripts/SFXPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour {
+
+    public AudioClip[] JumpingSounds;
+    public AudioClip[] SuperJumpingSounds;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playJumpingSound(bool isSuperJumping)
+    {
+        int randomIndex = Mathf.FloorToInt(Random.value * 3.99f);
+        if (isSuperJumping)
+        {
+            audioSource.clip = SuperJumpingSounds[randomIndex];
+            
+        }
+        else
+        {
+            audioSource.clip = JumpingSounds[randomIndex];
+        }
+        audioSource.Play();
+    }
+}
